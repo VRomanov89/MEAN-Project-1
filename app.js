08fa43a6f820345f20a5962bed15c7bb45d55ba6
Vladimir Romanov <vladromweb@gmail.com>
2017-02-04 16:53:39 -0800

    Adding Posts and uprooting functionality
    Adding Posts and uprooting functionality

--- app.js
+++ app.js
@@ -10,4 +10,23 @@ app.controller('MainCtrl', [
             {title: 'post 4', upvotes: 9},
             {title: 'post 5', upvotes: 4}
         ];
+
+        $scope.addPost = function(){
+            $scope.posts.push({title: 'A new post!', upvotes: 0});
+        };
+
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === '') { return; }
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0
+            });
+            $scope.title = '';
+            $scope.link = '';
+        };
+
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
     }]);
